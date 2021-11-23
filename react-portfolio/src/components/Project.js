ddedbdec2a827a1ec329b04a8f2a0efcb717235f
Andres Liu <andres.liu22@gmail.com>
2021-11-23 04:38:59 -0500

    Fix bugs

--- react-portfolio/src/components/Project.js
+++ react-portfolio/src/components/Project.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import githubLogo from '../assets/images/github-logo.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 export default function Project(props) {
     return (
